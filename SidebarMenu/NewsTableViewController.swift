17f21ff3eabf9f6666094543b915649a75f6b0a5
yeetuck <yyeetuck@gmail.com>
2017-08-06 17:42:12 -0700

    first commit

---
+++ SidebarMenu/NewsTableViewController.swift
@@ -0,0 +1,129 @@
+//
+//  NewsTableViewController.swift
+//  SidebarMenu
+//
+//  Created by Simon Ng on 2/2/15.
+//  Copyright (c) 2015 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewController: UITableViewController {
+    
+    @IBOutlet weak var menuButton:UIBarButtonItem!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        if self.revealViewController() != nil {
+            //SWRevealViewController provides the revealToggle: method to handle the expansion and contraction of the sidebar menu.
+            //set the target of the sidebar button to the reveal view controller and action to the revealToggle: method. So when the sidebar button is tapped, it’ll call the revealToggle: method to display the sidebar menu
+            menuButton.target = self.revealViewController()
+            menuButton.action = #selector(self.revealViewController().revealToggle(_:))
+            
+            //add a pan gesture recognizer
+            //swipe right in the content area to activate / reveal the sidebar, swipe left to hide the sidebar.
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+        
+        //works!
+        //self.revealViewController().rearViewRevealWidth = 262
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK - Table view data source
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // Return the number of sections.
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Return the number of rows in the section.
+        return 3
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! NewsTableViewCell
+        
+        // Configure the cell...
+        if indexPath.row == 0 {
+            cell.postImageView.image = UIImage(named: "watchkit-intro")
+            cell.postTitleLabel.text = "WatchKit Introduction: Building a Simple Guess Game"
+            cell.authorLabel.text = "Simon Ng"
+            cell.authorImageView.image = UIImage(named: "author")
+
+        } else if indexPath.row == 1 {
+            cell.postImageView.image = UIImage(named: "custom-segue-featured-1024")
+            cell.postTitleLabel.text = "Building a Chat App in Swift Using Multipeer Connectivity Framework"
+            cell.authorLabel.text = "Gabriel Theodoropoulos"
+            cell.authorImageView.image = UIImage(named: "appcoda-300")
+            
+        } else {
+            cell.postImageView.image = UIImage(named: "webkit-featured")
+            cell.postTitleLabel.text = "A Beginner’s Guide to Animated Custom Segues in iOS 8"
+            cell.authorLabel.text = "Gabriel Theodoropoulos"
+            cell.authorImageView.image = UIImage(named: "appcoda-300")
+            
+        }
+        
+        return cell
+    }
+    
+    
+    /**
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    **/
+    
+    /**
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    **/
+    
+    /**
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    **/
+    
+    /**
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    **/
+    
+    /**
+    //MARK - Navigation
+     
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    **/
+}
---
+++ SidebarMenu/PhotoViewController.swift
@@ -0,0 +1,46 @@
+//
+//  PhotoViewController.swift
+//  SidebarMenu
+//
+//  Created by Simon Ng on 2/2/15.
+//  Copyright (c) 2015 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+    
+    @IBOutlet weak var menuButton:UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if self.revealViewController() != nil {
+            //SWRevealViewController provides the revealToggle: method to handle the expansion and contraction of the sidebar menu.
+            //set the target of the sidebar button to the reveal view controller and action to the revealToggle: method. So when the sidebar button is tapped, it’ll call the revealToggle: method to display the sidebar menu
+            menuButton.target = self.revealViewController()
+            menuButton.action = #selector(self.revealViewController().revealToggle(_:))
+            
+            //add a pan gesture recognizer
+            //swipe right in the content area to activate / reveal the sidebar, swipe left to hide the sidebar.
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /**
+    //MARK - Navigation
+     
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    **/
+    
+}
